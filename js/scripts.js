97064dd1a42c89c633e7ae3181d237e66b624d8f
Isaiah <isaiah.adams7895@gmail.com>
2019-06-19 13:54:20 +0430

    struggle is real

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
 // 1: Set the text of the <h1> element
-
+const myHeading = document.querySelector("myHeading")
+myHeading.textContent = "Bucket List";
 
 // 2: Set the color of the <h1> to a different color
-
+myHeading.style.color = "red";
+    
 
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
-
-
+const desc = document.querySelector("desc")
+desc.innerHTML = "stuff thats <strong>fun<strong>"
 // 4: Set the class of the <ul> to 'list'
-
-
+const ulClass= document.querySelector("ul")
+ulClass.setAttribute("class","list")
 // 5: Create a new list item and add it to the <ul>
-
+const ulClass = document.querySelector("ul")
 
 // 6: Change all <input> elements from text fields to checkboxes
-
 
 
 // 7: Create a <button> element, and set its text to 'Delete'
